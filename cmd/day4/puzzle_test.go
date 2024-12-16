9ac16b8d7c2a59636c606b0463d67ab386ab12b8
Andrew Shoell <mrlunchbox777@gmail.com>
2024-12-15 23:49:36 -0700

    WIP something is broken

--- cmd/day4/puzzle_test.go
+++ cmd/day4/puzzle_test.go
@@ -9,6 +9,34 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	ms = Set{
+		Cell{Letter: "M"},
+		Cell{Letter: " "},
+		Cell{Letter: "S"},
+	}
+	a = Set{
+		Cell{Letter: " "},
+		Cell{Letter: "A"},
+		Cell{Letter: " "},
+	}
+	sm = Set{
+		Cell{Letter: "S"},
+		Cell{Letter: " "},
+		Cell{Letter: "M"},
+	}
+	mm = Set{
+		Cell{Letter: "M"},
+		Cell{Letter: " "},
+		Cell{Letter: "M"},
+	}
+	ss = Set{
+		Cell{Letter: "S"},
+		Cell{Letter: " "},
+		Cell{Letter: "S"},
+	}
+)
+
 // TestRotate90 is a test for the rotate90 function
 func TestRotate90(t *testing.T) {
 	testCases := []struct {
@@ -19,68 +47,18 @@ func TestRotate90(t *testing.T) {
 		{
 			name: "rotate90_1x3",
 			input: &Block{
-				Rows: Sets{
-					{
-						{Letter: "M"},
-						{Letter: " "},
-						{Letter: "S"},
-					},
-				},
+				Rows: Sets{ms},
 			},
 			expected: &Block{
-				Rows: Sets{
-					{
-						{Letter: "M"},
-					},
-					{
-						{Letter: " "},
-					},
-					{
-						{Letter: "S"},
-					},
-				},
+				Rows: Sets{sm},
 			},
 		},
 		{
 			name: "rotate90_3x3",
 			input: &Block{
-				Rows: Sets{
-					{
-						{Letter: "M"},
-						{Letter: " "},
-						{Letter: "S"},
-					},
-					{
-						{Letter: " "},
-						{Letter: "A"},
-						{Letter: " "},
-					},
-					{
-						{Letter: "M"},
-						{Letter: " "},
-						{Letter: "S"},
-					},
-				},
-			},
+				Rows: Sets{ms, a, ms}},
 			expected: &Block{
-				Rows: Sets{
-					{
-						{Letter: "M"},
-						{Letter: " "},
-						{Letter: "M"},
-					},
-					{
-						{Letter: " "},
-						{Letter: "A"},
-						{Letter: " "},
-					},
-					{
-						{Letter: "S"},
-						{Letter: " "},
-						{Letter: "S"},
-					},
-				},
-			},
+				Rows: Sets{mm, a, ss}},
 		},
 	}
 	for _, tc := range testCases {
@@ -99,26 +77,26 @@ func TestRotate90(t *testing.T) {
 			result, err := tc.input.rotate90(h, false)
 			// Assert
 			assert.Nil(t, err)
-			assert.Equal(t, result, tc.expected)
+			assert.Equal(t, tc.expected, result)
 			// Act
 			err = result.initialize(h, result.Rows)
 			// Assert
 			assert.Nil(t, err)
-			assert.Equal(t, result.Rows, tc.expected.Rows)
+			assert.Equal(t, tc.expected.Rows, result.Rows)
 			assert.Empty(t, tc.input.RRows)
-			assert.NotEqual(t, result.RRows, tc.input.RRows)
+			assert.NotEqual(t, tc.input.RRows, result.RRows)
 			assert.Empty(t, tc.input.Cols)
-			assert.NotEqual(t, result.Cols, tc.input.Cols)
+			assert.NotEqual(t, tc.input.Cols, result.Cols)
 			assert.Empty(t, tc.input.RCols)
-			assert.NotEqual(t, result.RCols, tc.input.RCols)
+			assert.NotEqual(t, tc.input.RCols, result.RCols)
 			assert.Empty(t, tc.input.ADiag)
-			assert.NotEqual(t, result.ADiag, tc.input.ADiag)
+			assert.NotEqual(t, tc.input.ADiag, result.ADiag)
 			assert.Empty(t, tc.input.RADiag)
-			assert.NotEqual(t, result.RADiag, tc.input.RADiag)
+			assert.NotEqual(t, tc.input.RADiag, result.RADiag)
 			assert.Empty(t, tc.input.DDiag)
-			assert.NotEqual(t, result.DDiag, tc.input.DDiag)
+			assert.NotEqual(t, tc.input.DDiag, result.DDiag)
 			assert.Empty(t, tc.input.RDDiag)
-			assert.NotEqual(t, result.RDDiag, tc.input.RDDiag)
+			assert.NotEqual(t, tc.input.RDDiag, result.RDDiag)
 		})
 	}
 }
